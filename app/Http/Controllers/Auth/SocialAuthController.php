bb7c1c64b11deeb5021b50a2d565646821056563
Nguyễn Quốc Bảo <637707@sv.vnua.edu.vn>
2021-10-12 16:05:28 +0700

    update

--- app/Http/Controllers/Auth/SocialAuthController.php
+++ app/Http/Controllers/Auth/SocialAuthController.php
@@ -6,56 +6,37 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Facades\URL;
-use Laravel\Socialite\Facades\Socialite;
-use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Response;
+use Laravel\Socialite\Facades\Socialite;
+
 class SocialAuthController extends Controller
 {
     public function getGoogleSignInUrl()
     {
-        try {
-            $url = Socialite::driver('google')->redirect()->getTargetUrl();
-            return response()->json([
-                'url' => $url,
-            ]);
-        } catch (\Exception $exception) {
-            return $exception;
-        }
+        return Socialite::driver('google')->redirect();
     }
 
     public function loginCallback(Request $request)
     {
-        try {
-            $state = $request->input('state');
 
-            parse_str($state, $result);
-            $googleUser = Socialite::driver('google')->user();
+        $googleUser = Socialite::driver('google')->user();
 
-            $user = User::where('email', $googleUser->email)->first();
-            if ($user) {
-                throw new \Exception(__('google sign in email existed'));
-            }
+        $user = User::where('google_id', $googleUser->id)->first();
+        if (!empty($user)) {
+            Auth::login($user);
+            return redirect()->route('home');
+        } else {
             $user = User::create(
                 [
                     'email' => $googleUser->email,
                     'name' => $googleUser->name,
-                    'google_id'=> $googleUser->id,
-                    'provider_name'=>'google',
+                    'google_id' => $googleUser->id,
+                    'provider_name' => 'google',
                 ]
             );
-            return response()->json([
-                'status' => __('google sign in successful'),
-                'data' => $user,
-            ]);
-
-        } catch (\Exception $exception) {
-            return response()->json([
-                'status' => __('google sign in failed'),
-                'error' => $exception,
-                'message' => $exception->getMessage()
-            ]);
         }
+        Auth::login($user);
+        return redirect()->route('home');
     }
 }
