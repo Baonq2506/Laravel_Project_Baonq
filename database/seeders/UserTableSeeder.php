6ec0ed5fbe41c5838005c8c348b11a91801814d9
Nguyễn Quốc Bảo <637707@sv.vnua.edu.vn>
2021-10-30 02:01:07 +0700

    update

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 class UserTableSeeder extends Seeder
 {
@@ -16,6 +17,20 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->truncate();
-        User::factory()->count(20)->create();
+        // User::factory()->count(20)->create();
+        $files = Storage::files('avatars/users');
+        $paths[] = '';
+        foreach ($files as $key => $file) {
+            $file = str_replace("avatars/", "", $file);
+            $paths[$key] = $file;
+        }
+        DB::table('users')->insert([
+            'name' => $this->faker->name(),
+            'status'=>rand(1,2),
+            'disk' =>'avatars',
+            'avatar' => $paths[rand(1,23)],
+            'email' => $this->faker->unique()->safeEmail(),
+            'password' => bcrypt('123456789'),
+        ]);
     }
 }
