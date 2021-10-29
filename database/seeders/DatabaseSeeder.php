12a1953aa3c8cb749b9eb6bc7114289919de760a
Nguyễn Quốc Bảo <637707@sv.vnua.edu.vn>
2021-10-30 00:42:58 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,6 @@ class DatabaseSeeder extends Seeder
         $this->call(TagsTableSeeder::class);
         $this->call(userSocialNetworkTableSeeder::class);
         $this->call(RolePermissionsTableSeeder::class);
-        $this->call(userInfosTableSeeder::class);
+        // $this->call(userInfosTableSeeder::class);
     }
 }
