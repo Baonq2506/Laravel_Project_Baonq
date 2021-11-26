cc72e0851db63a5beccde929e0dd6432f49dab9e
Nguyễn Quốc Bảo <637707@sv.vnua.edu.vn>
2021-11-27 00:29:06 +0700

    update

--- database/seeders/PostTableSeeder.php
+++ database/seeders/PostTableSeeder.php
@@ -5,7 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use App\Models\Post;
-
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Faker\Factory as Faker;
 class PostTableSeeder extends Seeder
 {
     /**
@@ -15,7 +17,31 @@ class PostTableSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Faker::create();
         DB::table('posts')->truncate();
         Post::factory()->count(303)->create();
+        for($i=0;$i<30;$i++){
+        $fake = $faker->sentence(3);
+        $files = Storage::files('public/Blogs');
+        $paths[] = '';
+        foreach ($files as $key => $file) {
+            $file = str_replace("public/", "", $file);
+            $paths[$key] =  $file;
+        }
+        $pathID=rand(1,20);
+        DB::table('posts')->insert([
+            'title' => $fake,
+            'slug' => Str::slug($fake),
+            'disk' => 'public',
+            'image_url' =>$paths[$pathID],
+            'content' => $faker->text($maxNbChars = 500),
+            'user_created_id' => rand(1, 50),
+            'user_updated_id' => rand(1, 10),
+            'category_id' => rand(1, 4),
+            'status' => rand(1, 3),
+            'created_at' =>  $faker->datetime()->format('Y-m-d H:i:s'),
+            'updated_at' => $faker->datetime()->format('Y-m-d H:i:s'),
+        ]);
+    }
     }
 }
